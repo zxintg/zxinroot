28b6495719b1a44cc65e7c4c2cf7815db80c2de4
1026378750 <1026378750@qq.com>
2019-10-13 15:29:33 +0800

    优化

--- library/src/main/java/com/zxin/root/util/FileUtil.java
+++ library/src/main/java/com/zxin/root/util/FileUtil.java
@@ -928,7 +928,7 @@ public final class FileUtil {
         cleanLogFile(path);
     }
 
-    public static void cleanLogFile(final String path) {
+    public void cleanLogFile(final String path) {
         ThreadManager.post(
                 new Runnable() {
                     @Override
@@ -954,5 +954,27 @@ public final class FileUtil {
                         }
                     }
                 });
+    }
+
+    /**
+     * 递归删除目录下的所有文件及子目录下所有文件
+     *
+     * @param dir 将要删除的文件目录
+     * @return boolean Returns "true" if all deletions were successful.
+     * If a deletion fails, the method stops attempting to
+     * delete and returns "false".
+     */
+    public boolean deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        // 目录此时为空，可以删除
+        return dir.delete();
     }
 }
--- library/src/main/java/com/zxin/root/util/UiUtils.java
+++ library/src/main/java/com/zxin/root/util/UiUtils.java
@@ -8,6 +8,12 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
 import android.content.res.Resources;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.zxin.root.R;
 import com.zxin.root.view.CustomGridLayoutManager;
@@ -19,6 +25,12 @@ import java.util.List;
 import java.util.Locale;
 
 public class UiUtils {
+    private static final String TYPE_DRAWABLE = "drawable";
+    private static final String TYPE_LAYOUT = "layout";
+    private static final String TYPE_STRING = "string";
+    private static final String TYPE_DIMEN = "dimen";
+    private static final String TYPE_ANIM = "anim";
+    private static final String TYPE_ID = "id";
 
     public enum LayoutManager{
         HORIZONTAL,
@@ -91,7 +103,7 @@ public class UiUtils {
      *
      */
     public String getFormatString(int strRes ,Object str){
-        return String.format(mContext.getResources().getString(strRes), str);
+        return String.format(getResources().getString(strRes), str);
     }
 
 
@@ -134,13 +146,12 @@ public class UiUtils {
     }
 
     public float getDimension(int resId) {
-        return mContext.getResources()
-                .getDimension(resId);
+        return getResources().getDimension(resId);
     }
 
     public String getFromAssets(String fileName) {
         try {
-            InputStream paramString = mContext.getResources().getAssets().open(fileName);
+            InputStream paramString = getResources().getAssets().open(fileName);
             byte[] arrayOfByte = new byte[paramString.available()];
             paramString.read(arrayOfByte);
             String txt = new String(arrayOfByte, "UTF-8");
@@ -160,10 +171,125 @@ public class UiUtils {
     }
 
     public float getCornerRadius(int dimen){
-       return mContext.getResources().getDimensionPixelOffset(dimen);
+       return getResources().getDimensionPixelOffset(dimen);
     }
 
     public String[] getStringArray(int resId){
-       return mContext.getResources().getStringArray(resId);
+       return getResources().getStringArray(resId);
+    }
+
+
+    /**
+     * 设置textView显示局部字体颜色改变
+     *
+     * @param builder 源数据
+     * @param color   字体颜色
+     * @param start   文字开始下标
+     * @param len     修改文字长度
+     * @return 修改后的数据源
+     */
+    public static SpannableStringBuilder setTextPartColor(SpannableStringBuilder builder
+            , int color
+            , int start
+            , int len) {
+        ForegroundColorSpan csp = new ForegroundColorSpan(color);
+        builder.setSpan(csp, start, start + len, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        return builder;
+    }
+
+    /*****
+     * 设置textView显示局部字体点击事件
+     * @param builder 源数据
+     * @param listener 回调函数
+     * @param start 文字开始下标
+     * @param len 修改文字长度
+     * @return 修改后的数据源
+     */
+    public static SpannableStringBuilder setTextPartClick(final SpannableStringBuilder builder
+            , final View.OnClickListener listener
+            , int start
+            , int len) {
+
+        //设置局部可点击事件
+        builder.setSpan(new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                //局部点击的响应事件
+                if (listener == null) {
+                    return;
+                }
+                listener.onClick(view);
+            }
+        }, start, start + len, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        return builder;
+    }
+
+
+    /****
+     * 根据ID获取Dimen
+     * @param resId 资源id
+     * @return
+     */
+    public int getDimensionById(int resId) {
+        return getResources().getDimensionPixelSize(resId);
+    }
+
+    /****
+     * 根据name获取Dimen
+     * @param resName
+     * @return
+     */
+    public int getDimensionByName(String resName) {
+        int resId = getDimenId(resName);
+        return getDimensionById(resId);
+    }
+
+    /****
+     * 获取布局
+     * @param id
+     * @param <V>
+     * @return
+     */
+    public <V extends View> V getViewById(int id) {
+        return (V) LayoutInflater.from(mContext).inflate(id, null);
+    }
+
+    /*****
+     * 厘米转换px
+     * 72dpi 1厘米
+     * @param cm 单位 厘米
+     * @return
+     */
+    public float cm2px(float cm){
+        float scale = getResources().getDisplayMetrics().density;
+        return cm * 72 * scale + 0.5f;
+    }
+
+    public int getIdentifierId(String name) {
+        return getResourceId(name, TYPE_ID);
+    }
+
+    public int getDrawableId(String name) {
+        return getResourceId(name, TYPE_DRAWABLE);
+    }
+
+    public int getStringId(Context context, String name) {
+        return getResourceId(name, TYPE_STRING);
+    }
+
+    public int getLayoutId(String name) {
+        return getResourceId(name, TYPE_LAYOUT);
+    }
+
+    public int getDimenId(String name) {
+        return getResourceId(name, TYPE_DIMEN);
+    }
+
+    public int getAnimId(String name) {
+        return getResourceId(name, TYPE_ANIM);
+    }
+
+    public int getResourceId(String name, String defType) {
+        return getResources().getIdentifier(name,defType,mContext.getPackageName());
     }
 }
